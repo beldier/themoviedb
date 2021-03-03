91538ec61fc8020d72fff5c457c627d502739c7d
Alvaro <alvaro.yapu.cossio@gmail.com>
2021-03-03 18:36:22 -0400

    feature #9: Add imagebutton

--- app/src/main/java/com/example/movieappfrontend/ui/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieappfrontend/ui/moviedetail/MovieDetailFragment.kt
@@ -1,6 +1,8 @@
 package com.example.movieappfrontend.ui.moviedetail
 
+import android.graphics.Color.green
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +10,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.example.movieappfrontend.R
@@ -26,6 +30,8 @@ const val MOVIE_OVERVIEW = "extra_movie_overview"
 
 
 class MovieDetailFragment : Fragment() {
+
+    private lateinit var viewModel : MovieDetailViewModel
     private lateinit var backdrop: ImageView
     private lateinit var poster: ImageView
     private lateinit var title: TextView
@@ -33,11 +39,13 @@ class MovieDetailFragment : Fragment() {
     private lateinit var releaseDate: TextView
     private lateinit var overview: TextView
     private lateinit var genres: TextView
+    private var isFavourite = false
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentMovieDetailBinding>(
             inflater, R.layout.fragment_movie_detail, container, false
         )
+        viewModel = ViewModelProvider(this).get(MovieDetailViewModel::class.java)
         backdrop = binding.movieBackdrop
         poster = binding.moviePoster
         title = binding.movieTitle
@@ -46,9 +54,14 @@ class MovieDetailFragment : Fragment() {
         overview = binding.movieOverview
         genres = binding.movieGenres
 
+        binding.favouriteButton.setOnClickListener{
+            isFavourite = !isFavourite
+            if(isFavourite)
+                binding.favouriteButton.setImageResource(android.R.drawable.btn_star_big_on)
+            else
+                binding.favouriteButton.setImageResource(android.R.drawable.btn_star_big_off)
+        }
         populateDetails()
-
-
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/movieappfrontend/ui/moviedetail/MovieDetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.movieappfrontend.ui.moviedetail
+
+import androidx.lifecycle.ViewModel
+
+class MovieDetailViewModel : ViewModel() {
+    init {
+
+    }
+}
